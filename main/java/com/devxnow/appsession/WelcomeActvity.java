e9255085ad674140df60063cd014bc69bb764873
College Innovators <40389731+CollegeInnovators@users.noreply.github.com>
2021-11-26 15:39:55 +0530

    Initial Commit

---
+++ main/java/com/devxnow/appsession/WelcomeActvity.java
@@ -0,0 +1,14 @@
+package com.devxnow.appsession;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_actvity);
+    }
+}
